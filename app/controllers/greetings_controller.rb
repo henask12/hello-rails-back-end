d8159890569a6670ddce77a155d22aaab9e9ceee
Henok Mekonnen <Henokmekonnen5@gmail.com>
2023-11-01 23:27:31 +0300

    cors enabled

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random_greeting
-    @greeting = Message.order("RANDOM()").first
-    render json: @greeting
-  end
+    greeting = Message.order("RANDOM()").first
+    render json: greeting
+  end  
 end
